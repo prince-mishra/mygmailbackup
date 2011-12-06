0e4b0741eae28fba93f32b7cc39e7ccd8f79bcfa
U-dexter\prince <prince@dexter.(none)>
2011-12-06 22:41:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import imaplib, getpass
+def fetchMyMail():
+	try:
+		M = imaplib.IMAP4_SSL('imap.gmail.com',993)
+		M.login('indiandexter002@gmail.com','shakespeare')
+		M.select()
+		data = M.search(None, 'ALL')
+		info = M.fetch(495, '(RFC822)')
+		print info
+		M.close()
+		M.logout()
+	except Exception, fault:
+		raise fault
+	return
+if __name__=="__main__":
+		try:
+			print "hello"
+			fetchMyMail()
+		except Exception, fault:
+			raise fault
